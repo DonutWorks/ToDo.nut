0c5c100675fd5e55ed202739ce04fc313d86a053
김민혁 <minhyeok@gimminhyeog-ui-MacBook-Pro-2.local>
2014-08-03 16:27:26 +0900

    시각화를 했습니다. 하하

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,36 @@
 class WelcomeController < ApplicationController
-  def index
-    @todos = Todo.all
-  	@histories = History.all
-  end
+	def index
+		@todos = Todo.all
+		@histories = History.all
+
+		@data = decorate
+		# render plain: decorate
+
+		# output = File.open( "../assets/javascripts/test_data.json", w+)
+		# output << decorate
+		# output.close
+
+
+	end
+
+	private	
+	def decorate
+		data = {
+			"articles" => [],
+			"total" => 0,
+			"name" => "history name"
+		}
+
+		total = 0
+		
+		@todos.each do |t|
+			data["articles"].push([t.created_at.day,5])
+			total += 5
+		end
+
+		data["total"] = total
+
+		return [data].to_json
+
+	end
 end
