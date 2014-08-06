2f88a0aa523bdaec62adacc8680182d0080788f5
Young Hoon Kim <YoungHoonKim@Youngui-MacBook-Pro.local>
2014-08-06 17:24:01 +0900

    Add Project Managing feature_2

--- db/migrate/20140806075027_create_project_users.rb
+++ db/migrate/20140806075027_create_project_users.rb
@@ -1,9 +1,6 @@
 class CreateProjectUsers < ActiveRecord::Migration
   def change
     #drop_table :project_users
-    create_table :project_users do |t|
-
-      t.timestamps
-    end
+    
   end
 end
--- db/migrate/20140806075428_add_reference_project_user.rb
+++ db/migrate/20140806075428_add_reference_project_user.rb
@@ -1,10 +1,5 @@
 class AddReferenceProjectUser < ActiveRecord::Migration
   def change
-    drop_table :project_users
-    create_table :project_users do |t|
-      t.references :assigned_project, references: :project, index: true
-      t.references :assignee, references: :user, index: true
-      t.timestamps
-    end
+    
   end
 end
