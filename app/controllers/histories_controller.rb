25517922e0a5932530c327e9b2dd5f4082dbd180
Hong Chulju <ang0123dev@gmail.com>
2014-08-06 20:50:05 +0900

    transaction, exception handling added (#1)

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -15,14 +15,18 @@ class HistoriesController < ApplicationController
     @history = History.new(history_params)
     @history.user_id = current_user.id
 
-    if @history.save
-      associate_history_with_todos!
-      associate_history_with_assignees!
-      associate_history_with_images!
+    begin
+      @history.transaction do
+        associate_history_with_todos!
+        associate_history_with_assignees!
+        associate_history_with_images!
+        @history.save!        
+      end
       client.notify("히스토리가 추가되었어용 : #{@history.title} (#{Rails.application.routes.url_helpers.history_url(@history)})")
+      redirect_to root_path
+    rescue ActiveRecord::RecordInvalid
+      render 'new'
     end
-
-    redirect_to root_path
   end
 
   def show
@@ -40,13 +44,16 @@ class HistoriesController < ApplicationController
 
     @history = History.find(params[:id])
 
-    if @history.update(history_params)
-      associate_history_with_todos!
-      associate_history_with_assignees!
-      associate_history_with_images!
+    begin
+      @history.transaction do
+        associate_history_with_todos!
+        associate_history_with_assignees!
+        associate_history_with_images!
+        @history.update!(history_params)
+      end
       client.notify("히스토리가 수정되었어용 : #{@history.title} (#{Rails.application.routes.url_helpers.history_url(@history)})")
       redirect_to @history
-    else
+    rescue ActiveRecord::RecordInvalid
       render 'edit'
     end
   end
@@ -67,30 +74,21 @@ class HistoriesController < ApplicationController
   def associate_history_with_todos!
     @history.todos.destroy_all
     params[:history][:todo_ids].select!(&:present?).each do |id|
-      history_todo = HistoryTodo.new
-      history_todo.history_id = @history.id
-      history_todo.todo_id = id
-      history_todo.save
+      @history.history_todos.build(todo_id: id)
     end
   end
 
   def associate_history_with_assignees!
     @history.assignees.destroy_all
     params[:history][:assignee_ids].select!(&:present?).each do |id|
-      history_user = HistoryUser.new
-      history_user.assigned_history_id = @history.id
-      history_user.assignee_id = id
-      history_user.save
+      @history.history_users.build(assignee_id: id)
     end
   end
 
   def associate_history_with_images!
     @history.images.destroy_all
     params[:history][:images].each do |image|
-      image_param = ActionController::Parameters.new({:image => image})
-      history_image = HistoryImage.new(image_param.permit(:image))
-      history_image.history_id = @history.id
-      puts "saved image: #{history_image.save}"
+      @history.images.build(image: image)
     end
   end
 end
