25230345c951dd991104268200054bca8c8d3b4f
Hong Chulju <ang0123dev@gmail.com>
2014-08-17 22:07:32 +0900

    render :json is refactored with ActiveModel::Serializer

--- config/routes.rb
+++ config/routes.rb
@@ -16,27 +16,24 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
 
   resources :projects do
-
     resources :todos do
       collection do
-        get 'list(/:id)', action: 'list'
+        get 'list(/:id)', action: 'list', defaults: {format: 'json'}
       end
     end
 
     resources :histories do
       collection do
-        get 'list_members' # should move to projects#members
-        get 'list(/:id)', action: 'list'
+        get 'list_members', defaults: {format: 'json'} # should move to projects#members
+        get 'list(/:id)', action: 'list', defaults: {format: 'json'}
       end
       resources :comments
     end
     
     collection do
       get '(/:project_id)/main', action: 'main', as: 'main'
-      
-
-      
     end
+
     get 'members(/:nickname)', action: 'members', as: 'members'
   end
 
