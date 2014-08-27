cba7c310f454bd31388c462bf1794869116dfe9b
Mujin Chae <chaesocool@gmail.com>
2014-08-28 00:10:21 +0900

    resolve merge conflict in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140825161129) do
-=======
-ActiveRecord::Schema.define(version: 20140824162727) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -32,7 +29,6 @@ ActiveRecord::Schema.define(version: 20140824162727) do
   add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
   add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
   add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
->>>>>>> master
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
