f3660d60f21ce1b1fdad6902cdf3cf4defe83974
Hong ChulJu <ang0123dev@gmail.com>
2014-08-30 00:52:57 +0900

    drop readable_type length limit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827051433) do
+ActiveRecord::Schema.define(version: 20140829154433) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -117,8 +117,8 @@ ActiveRecord::Schema.define(version: 20140827051433) do
 
   create_table "read_marks", force: true do |t|
     t.integer  "readable_id"
-    t.integer  "user_id",                  null: false
-    t.string   "readable_type", limit: 20, null: false
+    t.integer  "user_id",       null: false
+    t.string   "readable_type", null: false
     t.datetime "timestamp"
   end
 
