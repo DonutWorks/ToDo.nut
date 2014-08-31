0a5ac8c1cb8d7a8c16b63fcfcc85db556a943437
Hong ChulJu <ang0123dev@gmail.com>
2014-08-31 18:00:11 +0900

    Refactor HistoryController, History(model)

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,7 +1,8 @@
 class History < ActiveRecord::Base
 
-
   before_create :phistory_counter_increment
+  before_save :associate_histories!, :associate_todos!
+  after_save :send_notifications!
   
   has_many :history_todos
   has_many :todos, through: :history_todos
@@ -29,14 +30,49 @@ class History < ActiveRecord::Base
 
   def self.fetch_list_from(phistory_id, count)
     where(arel_table[:phistory_id].gteq(phistory_id)).take(count)
+  end
 
+  def attach_images!(images)
+    self.images.destroy_all
+    images.each do |image|
+      images.build(image: image)
+    end unless images.nil?
+  end
+
+  def assign_users_with_ids!(user_ids)
+    assignees.destroy_all
+    user_ids.each do |user_id|
+      history_users.build(assignee_id: user_id)
+    end unless user_ids.nil?
   end
 
   private
     def phistory_counter_increment
       self.phistory_id = self.project.phistory_counter + 1
       self.project.update(:phistory_counter => self.project.phistory_counter + 1)
-      
     end
 
+    def associate_histories!
+      referenced_histories = ReferenceCheck::ForHistory.references(project, description)
+      referencing_histories.destroy_all
+      referenced_histories.each do |history|
+        history_histories.build(referencing_history_id: history.id)
+      end
+    end
+
+    def associate_todos!
+      referenced_todos = ReferenceCheck::ForTodo.references(project, description)
+      todos.destroy_all
+      referenced_todos.each do |todo|
+        history_todos.build(todo_id: todo.id)
+      end
+    end
+
+    def send_notifications!
+      mentioned_users = description.scan(/\B@([^\s]+)\b/).flatten!.to_a
+      mentioned_users.map! { |nickname| User.find_by_nickname(nickname) }
+      mentioned_users.each do |user|
+        create_activity(action: 'mention', recipient: user, owner: self.user)
+      end
+    end
 end
